6bc34c17097219bdbed93f0c6e5a323b71f0e520
eylen <saioapicado@gmail.com>
2017-11-28 20:33:52 +0100

    completed

---
+++ app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -0,0 +1,18 @@
+package com.example.android.sunshine;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+    }
+}
